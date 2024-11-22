73be336681868bbebfa5cefd77b460fb2e8b1cde
aswanth-team <travellbuddyfinder@gmail.com>
2024-11-22 15:00:13 +0530

    DAY11-1

--- lib/user/screens/users.dart
+++ lib/user/screens/users.dart
@@ -2,7 +2,7 @@ final List<Map<String, dynamic>> users = [
   {
     "userImage": "assets/profile/aswanth.webp",
     "userName": "aswanth123",
-    "userFullName": "Aswanth Kumar",
+    "userFullName": "Aswanth K",
     "userBio":
         "Lover of nature and travel. Always exploring new places and capturing memories. I believe in living life to the fullest. Come join my journey!",
     "userGender": "Male",
@@ -10,66 +10,60 @@ final List<Map<String, dynamic>> users = [
     "userLocation": ["kerala", "kannur"],
     "userPosts": [
       {
-        "postId": "1",
-        "tripLocation": "New York",
+        "postId": "post1",
+        "tripLocation": "Ooty",
         "tripLocationDescription":
-            "The city that never sleeps. Amazing places to visit!",
+            "Ooty is a resort town in the Western Ghats mountains, in southern India's Tamil Nadu state.",
         "locationImages": [
-          "assets/bg3.jpg",
-          "assets/bg4.jpg",
-          "assets/bg4.jpg",
+          "assets/locimage/oo.jfif",
+          "assets/locimage/ooty.webp",
+          "assets/locimage/ootya.jpg",
         ],
         "tripCompleted": true,
         "tripDuration": 5,
         "tripRating": 4.5,
         "tripBuddies": [
           'sagar123',
-          'sagar123',
-          'aswanth123',
-          'sagar123',
-          'sagar123',
-          'aswanth123'
+          'ajmal12',
+          'vyshnav221',
+          
         ],
-        "tripFeedback": "nice",
-        "planToVisitPlaces": ['kollam', 'tesla', 'park'],
+        "tripFeedback": "Nice Better Experience For Me",
+        "planToVisitPlaces": ['Toy Train', 'Ooty Lake', ' Ooty Botanical Gardens','Ooty Rose Gardens','Thread Garden','Doddabetta Peak','Dolphins Nose'],
         "visitedPlaces": [
-          'Kannur',
-          'kollam',
-          'kottayam',
-          'kochi',
-          'goa',
-          'bangolore',
-          'eranad',
-          'wayanad'
+          'Toy Trainr',
+          'Doddabetta Peak',
+          'Ooty Botanical Gardens',
+          
         ],
       },
       {
-        "postId": "2",
-        "tripLocation": "New York",
+        "postId": "post2",
+        "tripLocation": "Mount Fuji",
         "tripLocationDescription":
-            "The city that never sleeps. Amazing places to visit!",
+            "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo",
         "tripDuration": 3,
         "locationImages": [
-          "assets/bg2.jpg",
-          "assets/b4.jpg",
-          "assets/bg4.jpg",
+          "assets/locimage/mountfuji1.webp",
+          "assets/locimage/mountfuji2.jpg",
+          "assets/locimage/mountfuji3.jfif",
         ],
         "tripCompleted": false,
         "tripRating": null,
-        "tripBuddies": [],
+        "tripBuddies": null,
         "tripFeedback": null,
-        "planToVisitPlaces": ['kollam', 'park', 'lighthouse'],
-        "visitedPlaces": [],
+        "planToVisitPlaces":['Subashiri 5th Station','Fujinomiya 5th Station','Snow Town Yeti','Lake Kawaguchiko'],
+        "visitedPlaces": null,
       }
     ],
     "tripPhotos": [
       'assets/profile/aswanth.webp',
-      "assets/bg2.jpg",
-      "assets/bg4.jpg",
-      "assets/bg4.jpg",
+      "assets/locimage/mountfuji22.jfif",
+      "assets/locimage/mountfuji33.jpg",
+      "assets/locimage/mountfuji44.jfif",
     ],
     "userSocialLinks": {
-      "instagram": "https://www.instagram.com/aswanth123",
+      "instagram": "https://www.instagram.com/a.swnth_",
       "facebook": "https://www.facebook.com/aswanth.kumar",
       "gmail": "aswanth.kumar@gmail.com",
       "twitter": "https://x.com/__x"
@@ -78,55 +72,60 @@ final List<Map<String, dynamic>> users = [
   {
     "userImage": "assets/profile/sagar.jpg",
     "userName": "sagar123",
-    "userFullName": "Sagar Sree",
+    "userFullName": "Sagar",
     "userBio":
         "Lover of nature and travel. Always exploring new places and capturing memories. I believe in living life to the fullest. Come join my journey!",
     "userGender": "Male",
-    "userDOB": "January 1, 1995",
+    "userDOB": "December 5, 2003",
     "userLocation": ["Tamilnadu", "chennai"],
     "userPosts": [
       {
-        "postId": "1",
-        "tripLocation": "New York",
+        "postId": "post1",
+        "tripLocation": "vattavada",
         "tripLocationDescription":
             "The city that never sleeps. Amazing places to visit!",
         "locationImages": [
-          "assets/bg2.jpg",
-          "assets/bg3.jpg",
-          "assets/bg4.jpg",
+          "assets/locimage/vattavada1.jpg",
+          "assets/locimage/vattavada2.jpg",
+          "assets/locimage/vattavada4.jpg",
         ],
         "tripCompleted": true,
         "tripDuration": 6,
         "tripRating": 4.5,
-        "tripBuddies": [],
+        "tripBuddies": [
+          'aswanth123',
+          'ajmal12',
+          'vyshnav221',
+          ],
         "tripFeedback": "nice",
-        "planToVisitPlaces": [],
-        "visitedPlaces": [],
+        "planToVisitPlaces": ['Mattupetty Dam', 'Top Station', 'Pampadum Shola National Park', 'Vattavada Beauty View Point',' Strawberry Farm',' Pazhathottam View Point',' Chilanthiyar Waterfall'],
+        "visitedPlaces": ['Mattupetty Dam','Top Station','Vattavada Beauty View Point'],
       },
       {
-        "postId": "2",
-        "tripLocation": "New York",
+        "postId": "post2",
+        "tripLocation": "Manali",
         "tripLocationDescription":
-            "The city that never sleeps. Amazing places to visit!",
+            "Manali is a high-altitude Himalayan resort town in India’s northern Himachal Pradesh state.",
         "tripDuration": 7,
         "locationImages": [
-          "assets/bg2.jpg",
-          "assets/bg3.jpg",
-          "assets/bg4.jpg",
+          "assets/locimage/manali1.jfif",
+          "assets/locimage/manali2.jfif",
+          "assets/locimage/manali3.jpg",
+          "assets/locimage/manali4.jpg",
         ],
         "tripCompleted": false,
         "tripRating": null,
-        "tripBuddies": [],
+        "tripBuddies": null,
         "tripFeedback": null,
-        "planToVisitPlaces": [],
-        "visitedPlaces": [],
+        "planToVisitPlaces":['Hadimba Mata Temple','Solang','Old Manali','Hidimba Devi Temple','Jogini Waterfall','Shri Anjani Mahadev Mandir'],
+        "visitedPlaces":null,
       }
     ],
     "tripPhotos": [
       'assets/profile/sagar.jpg',
-      "assets/bg2.jpg",
-      "assets/bg4.jpg",
-      "assets/bg4.jpg",
+      "assets/locimage/manali11.avif",
+      "assets/locimage/manali22.avif",
+      "assets/locimage/manali33.avif",
     ],
     "userSocialLinks": {
       "instagram": "https://www.instagram.com/aswanth123",
@@ -134,6 +133,147 @@ final List<Map<String, dynamic>> users = [
       "gmail": "aswanth.kumar@gmail.com",
       "twitter": "https://x.com/__x"
     },
-  },
+  }, {
+    "userImage": "assets/profile/vyshnav.png",
+    "userName": "Vyshnav231",
+    "userFullName": "Vyshnav Das ",
+    "userBio":
+        "Lover of nature and travel. Always exploring new places and capturing memories. I believe in living life to the fullest. Come join my journey!",
+    "userGender": "male",
+    "userDOB": "January 30 2004",
+    "userLocation": ["kerala", "kannur"],
+    "userPosts": [
+      {
+        "postId": "1",
+        "tripLocation": "Lakshadweep",
+        "tripLocationDescription":
+            "Lakshadweep is a tropical archipelago of 36 atolls and coral reefs in the Laccadive Sea, off the coast of Kerala, India.",
+        "locationImages": [
+          "assets/locimage/lakshadweep1.jpg",
+          "assets/locimage/lakshadweep2.jpg",
+          "assets/locimage/lakshadweep3.jpg",
+          "assets/locimage/lakshadweep4.webp",
+        ],
+        "tripCompleted": true,
+        "tripDuration": 5,
+        "tripRating": 4.5,
+        "tripBuddies": [
+          'sagar123',
+          'aswanth123',
+          
+        ],
+        "tripFeedback": "nice",
+        "planToVisitPlaces": [ 'Minicoy Island','Agatti Islands','Kalpeni Island','Bangaram Island','Marine Museum','Water Sports in Lakshadweep Islands',],
+        "visitedPlaces": [
+          'Minicoy Island',
+          'Agatti Islands',
+          'Kalpeni Island',
+        ],
+      },
+      {
+        "postId": "post2",
+        "tripLocation": "Andaman and Nicobar Islands",
+        "tripLocationDescription":
+            "The Andaman and Nicobar Islands are a union territory of India comprising 836 islands, of which only 31 are inhabited.",
+        "tripDuration": 3,
+        "locationImages": [
+          "assets/locimage/andamanandnicobarislands1.webp",
+          "assets/locimage/andamanandnicobarislands2.jpg",
+         "assets/locimage/andamanandnicobarislands3.jpg",
+         "assets/locimage/andamanandnicobarislands4.jpg",
+        ],
+        "tripCompleted": false,
+        "tripRating": null,
+        "tripBuddies": null,
+        "tripFeedback": null,
+        "planToVisitPlaces": ['Havelock Island', 'Neil island', 'Ross Island','North Bay Island','Baratang Island','Viper Island','Ross and Smith Island'],
+        "visitedPlaces" : null,
+      }
+    ],
+    "tripPhotos": [
+      'assets/profile/aswanth.webp',
+      "assets/locimage/andamanandnicobarislands11.jpg",
+       "assets/locimage/andamanandnicobarislands22.jpg",
+       "assets/locimage/andamanandnicobarislands33.webp",
+    ],
+    "userSocialLinks": {
+      "instagram": "https://www.instagram.com/aswanth123",
+      "facebook": "https://www.facebook.com/aswanth.kumar",
+      "gmail": "aswanth.kumar@gmail.com",
+      "twitter": "https://x.com/__x"
+    },
+  },{
+    "userImage": "assets/profile/adhravv.png",
+    "userName": "adhrav12",
+    "userFullName": "Adhrav ",
+    "userBio":
+        "Lover of nature and travel. Always exploring new places and capturing memories. I believe in living life to the fullest. Come join my journey!",
+    "userGender": "male",
+    "userDOB": "may 22 2004",
+    "userLocation": ["kerala", "kannur"],
+    "userPosts": [
+      {
+        "postId": "post1",
+        "tripLocation": "Pattaya City",
+        "tripLocationDescription":
+            "Pattaya City very beautiful city to visit",
+        "locationImages": [
+          "assets/locimage/pattayacity1.jpg",
+          "assets/locimage/pattayacity1.jpg",
+          "assets/locimage/pattayacity3.webp",
+          "assets/locimage/pattayacity4.webp",
+        ],
+        "tripCompleted": true,
+        "tripDuration": 5,
+        "tripRating": 4.5,
+        "tripBuddies": [
+          'sagar123',
+          'aswanth123',
+          'vyshnav231',
+          
+        ],
+        "tripFeedback": "nice",
+        "planToVisitPlaces": [ ' Nong Nooch Garden',' Art in Paradise Pattaya',' Sriracha Tiger Zoo',' Frost Magical Ice of Siam',' Pattaya Underwater World',' Pattaya Dolphinarium',' The Sanctuary of Truth','Elephant Jungle Sanctuary',],
+        "visitedPlaces": [
+          ' Nong Nooch Garden',
+          ' Sriracha Tiger Zoo',
+          ' Pattaya Underwater World',
+          ' The Sanctuary of Truth',
+          'Elephant Jungle Sanctuary',
+        ],
+      },
+      {
+        "postId": "post2",
+        "tripLocation": "Bali",
+        "tripLocationDescription":
+            "Bali is a province of Indonesia and the westernmost of the Lesser Sunda Islands.",
+        "tripDuration": 3,
+        "locationImages": [
+          "assets/locimage/bali1.jfif",
+          "assets/locimage/bali2.jfif",
+         "assets/locimage/bali3.jfif",
+         "assets/locimage/bali4.jfif",
+        ],
+        "tripCompleted": false,
+        "tripRating": null,
+        "tripBuddies": null,
+        "tripFeedback": null,
+        "planToVisitPlaces": ['Tanah Lot Temple', 'Tegallalang Rice Terrace', 'Saraswati Temple','Sanur','Nusa Dua Beach','Lovina Beach','Uluwatu Temple Bali Viewpoint'],
+        "visitedPlaces" : null,
+      }
+    ],
+    "tripPhotos": [
+      'assets/profile/aswanth.webp',
+      "assets/locimage/andamanandnicobarislands11.jpg",
+       "assets/locimage/andamanandnicobarislands22.jpg",
+       "assets/locimage/andamanandnicobarislands33.webp",
+    ],
+    "userSocialLinks": {
+      "instagram": "https://www.instagram.com/aswanth123",
+      "facebook": "https://www.facebook.com/aswanth.kumar",
+      "gmail": "aswanth.kumar@gmail.com",
+      "twitter": "https://x.com/__x"
+    },
+  }
 ];
 
