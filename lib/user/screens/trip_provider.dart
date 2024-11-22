2a8f02b47a6ceab970732defa0841f7ea70f4a0c
aswanth-team <travellbuddyfinder@gmail.com>
2024-11-22 15:21:31 +0530

    DAY11-3

---
+++ lib/user/screens/trip_provider.dart
@@ -0,0 +1,10 @@
+final List<Map<String, dynamic>> users = [
+ {'providerName' : 'makemytrip',
+ 'providerImage': 'assets/provider/makemytrip.png'},{'providerName' : 'booking',
+ 'providerImage': 'assets/provider/booking.png'},
+{'providerName' : 'tripadvisor',
+ 'providerImage': 'assets/provider/tripadvisor.png'},
+{'providerName' : 'traveltriangle',
+ 'providerImage': 'assets/provider/traveltriangle'},
+];
+
